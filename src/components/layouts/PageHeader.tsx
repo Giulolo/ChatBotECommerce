4678a8d2491c2bd04156e50a683aa83fd99089d1
Isabel Luo <kumatsundere@gmail.com>
2025-05-05 19:32:55 -0500

    adding header

---
+++ src/components/layouts/PageHeader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../../assets/img/cutecat_logo.gif"
+
+export function PageHeader() {
+  return (
+    <header 
+      className="bg-transparent text-primary p-3 transition-all flex gap-6" 
+    >
+      <img src={logo} alt="Logo" className="w-20 h-16 hover:opacity-50"  loading="lazy"  />
+      <nav className="w-screen flex items-center justify-center">
+        <ul className="flex flex-row items-center gap-12 text-lg">
+            <li><a href="/" className="hover:text-secondary">Home</a></li>
+            <li><a href="/product" className="hover:text-secondary">Productos</a></li>
+            <li><a href="/services" className="hover:text-secondary">Servicios</a></li>
+            <li><a href="/contact" className="hover:text-secondary">Contactos</a></li>
+        </ul>
+      </nav>
+
+      <img src="" alt="" className="w-0" loading="lazy" />
+    </header>
+  );
+} 
